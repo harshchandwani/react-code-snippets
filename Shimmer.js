2162219f7bd5f8dbe8fd989e2be39dc0f54876da
Harsh Chandwani <67815775+harshchandwani@users.noreply.github.com>
2023-10-05 10:53:29 +0530

    Create Shimmer.js

---
+++ Shimmer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Shimmer() {
+  return (
+    <div className='shimmer-container'>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+        <h1 className='shimmer-card'></h1>
+    </div>
+  )
+}
+
+export default Shimmer
